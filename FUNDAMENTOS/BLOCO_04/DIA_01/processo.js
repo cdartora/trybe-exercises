3fdfb994241cb1d2d39f6ddf0e0da2d75521213b
cdartora <cdartora>
2021-08-25 18:40:07 -0300

    Exercicio de switch/case

---
+++ FUNDAMENTOS/BLOCO_04/DIA_01/processo.js
@@ -0,0 +1,15 @@
+let candidato = 'aprovado';
+
+switch (candidato) {
+    case "aprovado":
+        console.log("aprovado");
+        break;
+    case "lista":
+        console.log("lista");
+        break;
+    case "reprovado":
+        console.log("reprovado");
+        break;
+    default:
+        console.log("não se aplica")
+}
