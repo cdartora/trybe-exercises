cd1653fc25a441aa5284c7ecfc37f217dc3836f9
cdartora <cdartora>
2021-08-27 01:50:20 -0300

    Exercicios de arrays

---
+++ FUNDAMENTOS/BLOCO_04/DIA_02/fixação/script.js
@@ -0,0 +1,16 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio);
+
+menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato")
+
+console.log(menu);
+
