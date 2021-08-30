4ea1d849d5cda6cc348c888d41072f7ade58c8d9
cdartora <cdartora>
2021-08-30 14:51:00 -0300

    Correções pequenas

--- FUNDAMENTOS/BLOCO_04/DIA_03/pyramids.js
+++ FUNDAMENTOS/BLOCO_04/DIA_03/pyramids.js
@@ -1,18 +1,18 @@
 // EXERCÍCIO 1
 let n = 11;
 
-// if (n > 1) {
-//     for (let i = 0; i < n; i+=1) {
-//         console.log('*'.repeat(n));
-//     }
-// }
+if (n > 1) {
+    for (let i = 0; i < n; i+=1) {
+        console.log('*'.repeat(n));
+    }
+}
 
 // EXERCÍCIO 2
-// if (n > 1) {
-//     for (let i = 1; i < n + 1; i+=1) {
-//         console.log('*'.repeat(i))
-//     }
-// }
+if (n > 1) {
+    for (let i = 1; i < n + 1; i+=1) {
+        console.log('*'.repeat(i))
+    }
+}
 
 // EXERCÍCIO 3
 // if (n > 1) {
@@ -31,23 +31,23 @@ let n = 11;
 // }
 
 // NÚMERO PRIMO
-let divisable = [];
-for (let i = 1; i <= n; i+=1) {
-    if (n % i == 0) {
-        divisable.push(i);
-    }
-}
-let checkHandler = 0;
-if(divisable.length == 2){
-    for (let i = 0; i < 2; i += 1) {
-        if (divisable[i] == 1 || divisable[i] == n) {
-            checkHandler += 1;
-        }  
-    }
-}
+// let divisable = [];
+// for (let i = 1; i <= n; i+=1) {
+//     if (n % i == 0) {
+//         divisable.push(i);
+//     }
+// }
+// let checkHandler = 0;
+// if(divisable.length == 2){
+//     for (let i = 0; i < 2; i += 1) {
+//         if (divisable[i] == 1 || divisable[i] == n) {
+//             checkHandler += 1;
+//         }  
+//     }
+// }
 
-if (checkHandler == 2) {
-    console.log('número primo')
-} else {
-    console.log('número não primo')
-}
+// if (checkHandler == 2) {
+//     console.log('número primo')
+// } else {
+//     console.log('número não primo')
+// }
