b3215312207b26a3f92b7f49b4bc1e045e3cbbf0
cdartora <cdartora>
2021-09-08 15:50:18 -0300

    Termina exercícios de fixação

--- FUNDAMENTOS/BLOCO_05/DIA_03/fixação/main.js
+++ FUNDAMENTOS/BLOCO_05/DIA_03/fixação/main.js
@@ -9,16 +9,49 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+const all = document.getElementsByTagName("*");
+
+firstLi.addEventListener("click", addTechClass);
+secondLi.addEventListener("click", addTechClass);
+thirdLi.addEventListener("click", addTechClass);
+
+function addTechClass (event) {
+  for (let i = 0; i < all.length; i += 1) {
+    if (all[i].className == 'tech') {
+      all[i].className = '';
+    }
+  }
+
+  event.target.className = 'tech';
+}
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener("keyup", changeText);
+
+function changeText () {
+  document.querySelector('.tech').innerText = input.value;
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+const header = document.querySelector("h1");
+header.addEventListener("dblclick", redirect);
+
+function redirect() {
+  window.open("https://cdartora.github.io/", "_blank");
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+firstLi.addEventListener("mouseover", changeColor);
+secondLi.addEventListener("mouseover", changeColor);
+thirdLi.addEventListener("mouseover", changeColor);
+
+function changeColor (e) {
+  e.target.style.backgroundColor = "grey";
+}
 
 // Segue abaixo um exemplo do uso de event.target:
 
