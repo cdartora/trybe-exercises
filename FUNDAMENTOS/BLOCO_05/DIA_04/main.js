bc75b4ae67d06adaa16fa9255ed98e53fb2efcdb
cdartora <cdartora>
2021-09-10 10:53:34 -0300

    Termina implementação do localStorage

--- FUNDAMENTOS/BLOCO_05/DIA_04/main.js
+++ FUNDAMENTOS/BLOCO_05/DIA_04/main.js
@@ -1,17 +1,21 @@
 const sepiaBg = document.querySelector("#sepia-bg");
 const blackBg = document.querySelector("#black-bg");
 const whiteBg = document.querySelector("#white-bg");
+const bodyStyle = document.body.style;
 
 sepiaBg.addEventListener("click", function() {
-    document.body.style.backgroundColor = "#f7e3cf";
+    bodyStyle.backgroundColor = "#f7e3cf";
+    LocalStorageHandler("background", "sepia");
 })
 
 blackBg.addEventListener("click", function() {
-    document.body.style.backgroundColor = "#3d3d3d";
+    bodyStyle.backgroundColor = "#3d3d3d";
+    LocalStorageHandler("background", "black");
 })
 
 whiteBg.addEventListener("click", function() {
-    document.body.style.backgroundColor = "#ffffff";
+    bodyStyle.backgroundColor = "#ffffff";
+    LocalStorageHandler("background", "white");
 })
 
 const sepiaFont = document.querySelector("#sepia-font");
@@ -19,39 +23,47 @@ const blackFont = document.querySelector("#black-font");
 const whiteFont = document.querySelector("#white-font");
 
 blackFont.addEventListener("click", function() {
-    document.body.style.color = "#000000";
+    bodyStyle.color = "#000000";
+    LocalStorageHandler("fontColor", "black")
 })
 whiteFont.addEventListener("click", function() {
-    document.body.style.color = "#ffffff";
+    bodyStyle.color = "#ffffff";
+    LocalStorageHandler("fontColor", "white")
 })
 sepiaFont.addEventListener("click", function() {
-    document.body.style.color = "#f7e3cf";
+    bodyStyle.color = "#f7e3cf";
+    LocalStorageHandler("fontColor", "sepia")
 })
 
 const moreBtn = document.querySelector("#more");
 const lessBtn = document.querySelector("#less");
-document.querySelector("main").style.fontSize = '20px';
+const mainStyle = document.querySelector("main").style;
+mainStyle.fontSize = '20px';
 
 moreBtn.addEventListener("click", function() {
-    let size = parseInt(document.querySelector("main").style.fontSize) + 1;
-    document.querySelector("main").style.fontSize = size + 'px';
+    let size = parseInt(mainStyle.fontSize) + 1;
+    mainStyle.fontSize = size + 'px';
+    LocalStorageHandler("fontSize", mainStyle.fontSize);
 })
 lessBtn.addEventListener("click", function() {
-    let size = parseInt(document.querySelector("main").style.fontSize) - 1;
-    document.querySelector("main").style.fontSize = size + 'px';
+    let size = parseInt(mainStyle.fontSize) - 1;
+    mainStyle.fontSize = size + 'px';
+    LocalStorageHandler("fontSize", mainStyle.fontSize);
 })
 
 const moreSpacing = document.querySelector("#more-spacing");
 const lessSpacing = document.querySelector("#less-spacing");
-document.querySelector("main").style.lineHeight = '20px'
+mainStyle.lineHeight = "20px";
 
 moreSpacing.addEventListener("click", function() {
-    let height = parseInt(document.querySelector("main").style.lineHeight) + 1;
-    document.querySelector("main").style.lineHeight = height + 'px';
+    let height = parseInt(mainStyle.lineHeight) + 1;
+    mainStyle.lineHeight = height + 'px';
+    LocalStorageHandler("lineHeight", mainStyle.lineHeight);
 })
 lessSpacing.addEventListener("click", function() {
-    let height = parseInt(document.querySelector("main").style.lineHeight) - 1;
-    document.querySelector("main").style.lineHeight = height + 'px';
+    let height = parseInt(mainStyle.lineHeight) - 1;
+    mainStyle.lineHeight = height + 'px';
+    LocalStorageHandler("lineHeight", mainStyle.lineHeight);
 })
 
 const verdana = document.querySelector("#verdana");
@@ -59,11 +71,55 @@ const arial = document.querySelector("#arial");
 const monospace = document.querySelector("#monospace");
 
 verdana.addEventListener("click", function () {
-    document.querySelector("main").style.fontFamily = "verdana";
+    mainStyle.fontFamily = "verdana";
+    LocalStorageHandler("fontFamily", "monospace");
 })
 arial.addEventListener("click", function () {
-    document.querySelector("main").style.fontFamily = "arial";
+    mainStyle.fontFamily = "arial";
+    LocalStorageHandler("fontFamily", "monospace");
 })
 monospace.addEventListener("click", function () {
-    document.querySelector("main").style.fontFamily = "monospace";
+    mainStyle.fontFamily = "monospace";
+    LocalStorageHandler("fontFamily", "monospace");
 })
+
+function LocalStorageHandler (func, propertie) {
+    if (localStorage.getItem(func) == "null") {
+        localStorage.setItem(func, propertie);
+    } else {
+        localStorage.removeItem(func);
+        localStorage.setItem(func, propertie);
+    }
+}
+
+
+
+window.onload =function() {
+    if (localStorage.getItem("background") == "sepia") {
+        bodyStyle.backgroundColor = "#f7e3cf";
+    } else if (localStorage.getItem("background") == "black") {
+        bodyStyle.backgroundColor = "#3d3d3d";
+    } else if (localStorage.getItem("background") == "white") {
+        bodyStyle.backgroundColor = "#ffffff";
+    }
+
+    if (localStorage.getItem("fontColor") == "black") {
+        bodyStyle.color = "#3d3d3d";
+    } else if (localStorage.getItem("fontColor") == "white") {
+        bodyStyle.color = "#ffffff";
+    } else if (localStorage.getItem("fontColor") == "sepia") {
+        bodyStyle.color = "#f7e3cf";
+    }
+
+    mainStyle.fontSize = localStorage.getItem("fontSize");
+
+    mainStyle.lineHeight = localStorage.getItem("lineHeight");
+
+    if (localStorage.getItem("fontFamily") == "verdana") {
+        mainStyle.fontFamily = "verdana";
+    } else if (localStorage.getItem("fontFamily") == "monospace") {
+        mainStyle.fontFamily = "monospace";
+    } else if (localStorage.getItem("fontFamily") == "arial") {
+        mainStyle.fontFamily = "arial";
+    }
+}
