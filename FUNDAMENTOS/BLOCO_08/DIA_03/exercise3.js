0748321dc3e3b74984df21e744a30ca33193beff
cdartora <cdartora>
2021-10-06 18:29:08 -0300

    termina exercício 3

--- FUNDAMENTOS/BLOCO_08/DIA_03/exercise3.js
+++ FUNDAMENTOS/BLOCO_08/DIA_03/exercise3.js
@@ -2,6 +2,7 @@ const books = [
   {
     id: 1,
     name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
     author: {
       name: 'George R. R. Martin',
       birthYear: 1948,
@@ -61,37 +62,39 @@ const books = [
 ];
 
 const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
+  { 
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
   },
   {
-    age: 38,
-    author: 'H. P. Lovecraft',
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
   },
   {
-    age: 39,
-    author: 'Stephen King',
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
   },
   {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
-  },
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
 ];
 
-function nameAndAge() {
+function fantasyOrScienceFiction() {
   // escreva seu código aqui
-  return books.map((book) => {
-    return { author: book.author.name, age: book.releaseYear - book.author.birthYear };
-  }).sort((a, b) => a.age - b.age)
+  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
 }
 
-console.log(nameAndAge());
+console.log(fantasyOrScienceFiction());
