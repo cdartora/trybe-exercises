268977747407f36b74b86035e9fa280cc2177aa0
cdartora <cdartora>
2021-11-03 16:26:21 -0300

    feat[fancy-button]: add button counter that toggle color

--- FRONT-END/BLOCO_12/DIA_01/fancy-button/src/App.js
+++ FRONT-END/BLOCO_12/DIA_01/fancy-button/src/App.js
@@ -4,30 +4,68 @@ class App extends React.Component {
   constructor() {
     super()
 
-    this.rightHandler = this.rightHandler.bind(this);
-    this.whatHandler = this.whatHandler.bind(this);
-    this.wrongHandler = this.wrongHandler.bind(this);
+    this.btnOneHandler = this.btnOneHandler.bind(this);
+    this.btnTwoHandler = this.btnTwoHandler.bind(this);
+    this.btnThreeHandler = this.btnThreeHandler.bind(this)
+
+    this.state = {
+      one: 0,
+      two: 0,
+      three: 0,
+    }
   }
 
-  rightHandler() {
-    console.log('youre right');
+  btnOneHandler() {
+    this.setState((previousState, props) => ({
+      one: previousState.one + 1,
+    }))
+
+    console.log(this.checkColor(this.state.one));
   }
 
-  whatHandler() {
-    console.log('what!?');
+  btnTwoHandler() {
+    this.setState((previousState, props) => ({
+      two: previousState.two + 1,
+    }))
+
+    console.log(this.checkColor(this.state.two));
   }
 
-  wrongHandler() {
-    console.log('youre very wrong');
+  btnThreeHandler() {
+    this.setState((previousState, props) => ({
+      three: previousState.three + 1,
+    }))
+
+    console.log(this.checkColor(this.state.three));
+  }
+
+  checkColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
   }
 
   render() {
+    const { one, two, three } = this.state;
     return (
       <div>
-        <h1>Qual o melhor pokemon?</h1>
-        <button onClick={ this.rightHandler }>Black/White</button>
-        <button onClick={ this.whatHandler }>Ruby/Sapphire</button>
-        <button onClick={ this.wrongHandler }>X/Y</button>
+        <h1>Botões</h1>
+
+        <button 
+          onClick={ this.btnOneHandler }
+          style={{ backgroundColor: this.checkColor(one) }}>
+            { this.state.one }
+        </button>
+
+        <button
+          onClick={ this.btnTwoHandler }
+          style={{ backgroundColor: this.checkColor(two) }}>
+            { this.state.two }
+        </button>
+
+        <button
+          onClick={ this.btnThreeHandler }
+          style={{ backgroundColor: this.checkColor(three) }}>
+            { this.state.three }
+        </button>
       </div>
     );
   }
